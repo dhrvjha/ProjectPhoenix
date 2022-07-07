985d16d2bf24b25e8a2ad7065abb97fcc63859d8
dhrvjha <kdhruv71@gmail.com>
2022-07-08 00:13:16 +0530

    added user portfolio view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,13 @@
-
+from django.contrib.auth.decorators import login_required
 from django.urls import path
-from portfolio.views import Home
+
+from portfolio.views import Home, UserPortfolioView
 
 urlpatterns = [
-    path("", Home.as_view(), name="home")
+    path("", Home.as_view(), name="home"),
+    path(
+        "<username:slug>/",
+        login_required(UserPortfolioView.as_view()),
+        name="portfolio_view",
+    ),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,61 @@
+from typing import Any, Dict
+
+from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
+
+from accounts.models import Account
 
 
 class Home(View):
     """Home view for {ALLOWED_HOST}"""
 
+    # TODO: design home.html
     template_name = "portfolio/home.html"
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class UserPortfolioView(View):
+    # TODO: create index.html and 404_error.html
+    """Portfolio of a logged in user
+
+    @info: Only logged in user can access this view
+    """
+
+    template_name = "portfolio/index.html"
+    not_found_template_name = "portfolio/404_error.html"
+    model = Account
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.request: HttpRequest
+        self.username: str
+
+    def get_queryset(self) -> Account:
+        return self.model.objects.get(username=self.username)
+
+    def get_context_data(self, **kwargs) -> Dict:
+        context = super().get_context_data(**kwargs)
+        try:
+            queryset = self.get_queryset()
+        except self.model.DoesNotExist():
+            return None
+        context["title"] = str(queryset)
+        context["first_name"] = queryset.first_name
+        context["last_name"] = queryset.last_name
+        context["email"] = queryset.email
+        context["username"] = queryset.username
+        context["profile_picture"] = queryset.profile_picture
+        return context
+
+    def get(self, request, username):
+        self.request = request
+        self.username = username
+        context = self.get_context_data()
+        if context is not None:
+            return render(request, self.template_name, context)
+        http_page = render(request, self.not_found_template_name, context)
+        http_page.status_code = 404
+        return http_page
