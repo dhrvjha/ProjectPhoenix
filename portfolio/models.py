b0cd4d343a47d74df656cee2b81a832ea7b6c1ae
dhrvjha <kdhruv71@gmail.com>
2022-07-09 03:06:25 +0530

    added reverse query support requirement

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE, related_name="portfolio"
+    )
+    css_reference_link = models.URLField(default="/static/css/style.css")
