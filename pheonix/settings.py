beedbbbfa087ada1b6f2279ab948b02e139b3eaf
dhrvjha <kdhruv71@gmail.com>
2022-07-09 03:10:09 +0530

    added support for static files

--- pheonix/settings.py
+++ pheonix/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = "/home/dhruv/dhrvjha.com/static"
+STATICFILES_DIRS = ["/home/dhruv/dhrvjha.com/www/static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
